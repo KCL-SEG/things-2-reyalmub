858c293e4d3fe6a31ad7dd0131f023e07cb6e458
Rey Almubarak <raghad.almub@gmail.com>
2023-12-08 01:58:48 +0000

    create things form and display the form in the home view

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,16 @@
+from django import forms
+from .models import Thing
+
+
 """Forms of the project."""
 
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        exclude = ['created_at']
+    widgets = {
+        'description': forms.Textarea(attrs={'rows': 4, 'cols': 40}), 
+        'quantity': forms.NumberInput(attrs={'min': 0, 'max': 100})
+    }
