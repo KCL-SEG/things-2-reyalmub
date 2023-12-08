5235b1b8e8fd478a6269df4f613a4dec3f95a8ce
Rey Almubarak <raghad.almub@gmail.com>
2023-12-08 02:03:51 +0000

    fix the errors in the tests

--- things/forms.py
+++ things/forms.py
@@ -4,13 +4,8 @@ from .models import Thing
 
 """Forms of the project."""
 
-# Create your forms here.
 class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
         fields = ['name', 'description', 'quantity']
-        exclude = ['created_at']
-    widgets = {
-        'description': forms.Textarea(attrs={'rows': 4, 'cols': 40}), 
-        'quantity': forms.NumberInput(attrs={'min': 0, 'max': 100})
-    }
+        widgets = {'description': forms.Textarea(), 'quantity': forms.NumberInput()}
